a1aa6c8d073c707dd5a082ddf162f50c98aa489d
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-09-23 09:09:41 +0100

    n

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import PageRoutes from "./Page/PageRoutes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Private } from "./Page/privateroute/Private";
-
-// import './main.scss';
+import React, { useState } from "react";
+import InputField from "./InputField";
+import "./styles.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleEmailChange = (newValue) => {
+    setEmail(newValue);
+
+    if (!isValidEmail(newValue)) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
+  };
+
+  const isValidEmail = (email) => {
+    return email.includes("@");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isValidEmail(email)) {
+      setSuccess(true);
+    } else {
+      setSuccess(false);
+    }
+  };
+
   return (
-    <BrowserRouter>
-      <ToastContainer />
-      <Routes>
-        <Route path="/*" exact element={<PageRoutes />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <InputField
+          label="Label"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(newValue) => setName(newValue)}
+          success={success}
+          isError={isError}
+        />
+      </form>
+    </div>
   );
 }
 
