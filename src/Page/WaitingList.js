90765e6133df43827649a6196f4f39984f4b401b
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-11-22 16:41:08 +0100

    initial commit for the responsivesness of the landing pages

--- src/Page/WaitingList.js
+++ src/Page/WaitingList.js
@@ -1,27 +1,16 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Header from "../Component/WaitingList_Component/Header";
-import Main from "../Component/WaitingList_Component/Main";
-
-import background from "../assets/images/medium-shot-man-delivering-groceries 1.jpg";
-import Footer from "../Component/WaitingList_Component/Footer";
+import Header from '../Component/WaitingList_Component/Header';
+import Main from '../Component/WaitingList_Component/Main';
 
 function WaitingList() {
   const [lang, setLang] = useState("en");
 
   return (
-    <div className="w-full h-screen overflow-hidden font-['Raleway']">
-      <div className="relative w-full h-full">
-        <img
-          className="object-cover w-full h-screen transition-all"
-          src={background}
-          alt=""
-        ></img>
-      </div>
-      <div className="absolute inset-0 overflow-hidden">
+    <div className="w-full h-screen font-['Raleway']">
+      <div className="absolute inset-0">
         <Header lang={lang} setLang={setLang} />
         <Main lang={lang} setLang={setLang} />
-        <Footer lang={lang} setLang={setLang} />
       </div>
     </div>
   );
