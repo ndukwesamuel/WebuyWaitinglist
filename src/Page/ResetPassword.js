0f91a082b4d96a5c0ec261480687ccd784bf8e6e
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-11-23 09:22:11 +0100

    fix all

--- src/Page/ResetPassword.js
+++ src/Page/ResetPassword.js
@@ -4,9 +4,10 @@ import background from "../assets/markus-spiske-ezYZfFnzARM-unsplash.jpg";
 
 import { toast } from "react-toastify";
 import axios from "axios";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate, Link } from "react-router-dom";
 import { usePasswordResetMutation } from "../Redux/PasswordResetApi";
-
+import { MdOutlineMail } from "react-icons/md";
+import { CiLock } from "react-icons/ci";
 const ResetPassword = () => {
   const [newPassword, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -51,80 +52,82 @@ const ResetPassword = () => {
 
   return (
     <div
-      className="relative flex content-center justify-center w-full h-screen md:px-6"
+      className=" min-h-screen py-10 px-2"
       style={{
         backgroundImage: `url(${background})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="flex content-center justify-center relative w-full h-screen">
-        <div className="flex content-center justify-center w-full h-full bg-no-repeat bg-cover bg-center relative z-10">
-          <div className="flex flex-row absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white w-[1000px] max-sm:w-full max-sm:bg-transparent rounded-xl rounded-l-xl h-[500px] z-40">
-            <div className="w-1/2 h-full max-sm:hidden bg-[#5F8575] rounded-l-xl">
-              <img
-                className="w-auto h-full ml-4 max-sm:hidden"
-                src={image}
-                alt=""
-              />
-            </div>
-            <div className="w-1/2 p-7 max-sm:p-0 max-sm:w-full">
-              <div className="w-[200px] max-sm:w-full flex flex-col content-center justify-center max-sm:-mt-3">
-                <h1 className="text-[#4F7942] text-[40px] max-sm:text-[#ffffff] max-sm:text-[40px] max-sm:text-center font-semibold">
-                  Reset Password
-                </h1>
-                <p className="text-[#4F7942] max-sm:text-[#ffffff] max-sm:text-[25px] max-sm:font-semibold opacity-70 max-sm:text-center">
-                  Enter your new password below.
-                </p>
-              </div>
-              <div
-                id="form"
-                className="flex flex-col rounded-md p-6 max-sm:mt-3 w-auto content-center justify-center"
-              >
-                <form
-                  action=""
-                  className="ml-8 max-sm:mx-auto content-center justify-center flex flex-col"
+      <div className=" md:flex md:justify-center">
+        <div className="bg-white   shadow-2xl flex justify-center   rounded-xl py-5 md:w-[50%]  lg:py-36 ">
+          <div>
+            <div className="text-center">
+              <p className="text-[30px] font-medium text-[#009B4D] lg:text-[40px]">
+                Reset Password
+              </p>
+              <p className="lg:text-[20px]">Enter your new password below.</p>
+              <p className="lg:text-[20px]">
+                You can{" "}
+                <Link
+                  to="/signup"
+                  className="text-[#009B4D] text-[15px] font-medium "
                 >
-                  <div
-                    id="password"
-                    className="flex flex-col gap-[4px] mb-3 max-sm:text-[#ffffff] max-sm:text-base"
-                  >
-                    <label htmlFor="password">New Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      value={newPassword}
-                      onChange={(e) => setPassword(e.target.value)}
-                      id="password"
-                      placeholder="Enter new password"
-                      className="border-b-[2px] rounded-[4px] p-1 pl-2 focus:border-b-[2px] active:border-[#4F7942] focus:outline-none outline-none w-[250px] text-[10px] border-[#4F7942] max-sm:text-sm max-sm:text-[#000000]"
-                    />
-                  </div>
-                  <div
-                    id="confirmPassword"
-                    className="flex flex-col gap-[4px] mb-3 max-sm:text-[#ffffff] max-sm:text-base"
-                  >
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input
-                      type="password"
-                      name="confirmPassword"
-                      value={confirmPassword}
-                      onChange={(e) => setConfirmPassword(e.target.value)}
-                      id="confirmPassword"
-                      placeholder="Confirm new password"
-                      className="border-b-[2px] rounded-[4px] p-1 pl-2 focus:border-b-[2px] active:border-[#4F7942] focus:outline-none outline-none w-[250px] text-[10px] border-[#4F7942] max-sm:text-sm max-sm:text-[#000000]"
-                    />
-                  </div>
-                  <button
-                    className="text-[#ffffff] hover:text-[#355E3B] max-sm:hover:text-[#ffffff] hover:bg-transparent hover:border-[1px] hover:border-[#355E3B] bg-[#355E3B] text-center px-[55px] py-[7px] max-sm:py-[10px] text-[14px] max-sm:text-[17px] rounded-full mx-auto"
-                    type="submit"
-                    onClick={handleSubmit}
-                  >
-                    Reset Password
-                  </button>
-                </form>
-              </div>
+                  Register here !{" "}
+                </Link>
+              </p>
             </div>
+
+            <form className=" flex-col gap-4 justify-center">
+              <div className="my-5">
+                <ReusableInput
+                  type="password"
+                  name="password"
+                  value={newPassword}
+                  onChange={(e) => setPassword(e.target.value)}
+                  id="password"
+                  placeholder="Enter new password"
+                  label="New Password"
+                  icon={CiLock}
+                />
+              </div>
+
+              <div className="my-5">
+                <ReusableInput
+                  type="password"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  id="confirmPassword"
+                  placeholder="Confirm new password"
+                  label="Confirm Password"
+                  icon={CiLock}
+                />
+              </div>
+
+              {/* <p className="mx-auto mt-3  "> */}
+              <Link
+                to="/forget-password"
+                className="text-[12px] flex justify-end cursor-pointer"
+              >
+                Forget Password ?
+              </Link>
+              {/* </p> */}
+
+              <button
+                className="text-[#ffffff] hover:text-[#355E3B] mt-10  hover:bg-transparent hover:border-[1px] hover:border-[#355E3B] bg-[#009B4D] text-center px-[55px] py-[7px]  text-[14px] rounded-[10px]  w-full"
+                type="button"
+                onClick={handleSubmit}
+              >
+                {isLoading ? (
+                  <div className="flex justify-center items-center">
+                    <div className="w-4 h-4 border-t-2 border-[#4f7942] border-solid rounded-full animate-spin" />
+                  </div>
+                ) : (
+                  <> Reset Password </>
+                )}
+              </button>
+            </form>
           </div>
         </div>
       </div>
@@ -132,3 +135,42 @@ const ResetPassword = () => {
   );
 };
 export default ResetPassword;
+const ReusableInput = ({
+  label,
+  type,
+  placeholder,
+  name,
+  value,
+  onChange,
+  icon: Icon,
+}) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    <div className="mb-3">
+      <label htmlFor="" className="block">
+        {label}
+      </label>
+      <div className="flex items-center gap-4 border-b-[1px] border-[#99999999]">
+        {Icon && <Icon />}
+        <input
+          className="outline-none w-full"
+          type={isPasswordVisible ? "text" : type}
+          placeholder={placeholder}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+        {type === "password" && (
+          <div onClick={togglePasswordVisibility} className="cursor-pointer">
+            {isPasswordVisible ? "👁️" : "🙈"}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
