e6c9ec7f931728d50ba871fcbc374737019257ab
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-11-23 07:29:00 +0100

    fix

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -8,6 +8,8 @@ import background from "../assets/markus-spiske-ezYZfFnzARM-unsplash.jpg";
 import ModalContainer from "../Component/modal-container/modal-container";
 import { Login_fun } from "../Redux/AuthenticationSlice";
 import { resetSignup } from "../Redux/Auth";
+import { MdOutlineMail } from "react-icons/md";
+import { CiLock } from "react-icons/ci";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -19,7 +21,6 @@ const Login = () => {
 
   const [loginform, setLoginform] = useState({
     email: "",
-
     password: "",
   });
 
@@ -39,6 +40,7 @@ const Login = () => {
       email: loginform?.email,
       password: loginform?.password,
     };
+    console.log({ newData });
     dispatch(Login_fun(newData));
   };
 
@@ -61,122 +63,142 @@ const Login = () => {
 
   return (
     <div
-      className="flex content-center justify-center relative w-full h-screen md:px-6"
+      className=" min-h-screen py-10 px-5"
       style={{
         backgroundImage: `url(${background})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="flex content-center justify-center w-full h-full  bg-no-repeat bg-cover bg-center relative z-10">
-        <div className="flex flex-row absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white w-[1000px] md:w-full xl:max-w-[1440px] max-sm:w-full max-sm:bg-transparent rounded-xl rounded-l-xl h-[500px] z-40">
-          <div className=" w-1/2 h-full max-sm:hidden bg-[#5F8575] rounded-l-xl">
-            <img
-              className=" w-auto h-full ml-4 max-sm:hidden"
-              src={image}
-              alt=""
-            ></img>
-          </div>
-          <div className="w-1/2 p-7 pl-10 max-sm:p-0 max-sm:w-full">
-            <div className="w-[200px] max-sm:w-full flex flex-col content-center justify-center max-sm:-mt-3">
-              <h1 className="text-[#4F7942] text-[40px] max-sm:text-[#ffffff] max-sm:text-[40px] max-sm:text-center font-semibold">
-                Welcome
-              </h1>
-              <p className="text-[#4F7942] max-sm:text-[#ffffff] max-sm:text-[25px] max-sm:font-semibold opacity-70 max-sm:text-center">
-                Let's get you started!
-              </p>
-            </div>
-            <div
-              id="form"
-              className="flex flex-col rounded-md py-3 max-sm:mt-3 w-auto content-center justify-center"
-            >
-              <form
-                action=""
-                className="max-sm:mx-auto content-center justify-center flex flex-col"
+      <div className="bg-white   shadow-2xl flex justify-center   rounded-xl py-5">
+        <div>
+          <div className="text-center">
+            <p className="text-[30px] font-medium text-[#009B4D]"> Sign in</p>
+            <p>If you don’t have an account register</p>
+            <p>
+              {" "}
+              You can{" "}
+              <Link
+                to="/signup"
+                className="text-[#009B4D] text-[15px] font-medium "
               >
-                <div
+                {" "}
+                Register here !{" "}
+              </Link>
+            </p>
+          </div>
+
+          <form className=" flex-col gap-4 justify-center">
+            {/* <div className=" mb-3">
+              <label htmlFor="" className="block">
+                {console.log({ email })}
+                Email
+              </label>
+              <div className="flex items-center gap-4 bor border-b-[1px] border-[#99999999] ">
+                <MdOutlineMail />
+                <input
+                  className="out outline-none w-full "
+                  type="text"
+                  placeholder="Enter your email address"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
                   id="email"
-                  className="flex flex-col gap-[4px] mb-3 max-sm:text-[#ffffff] max-sm:text-base"
-                >
-                  <label htmlFor="email">Email Address</label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={email}
-                    onChange={handleChange}
-                    id="email"
-                    placeholder="Youremail@gmail.com"
-                    className="border-b-[2px] rounded-[4px] p-1 pl-2 focus:border-b-[2px] active:border-[#4F7942] focus:outline-none outline-none w-[250px] text-[10px] border-[#4F7942] max-sm:text-sm max-sm:text-[#000000]"
-                  ></input>
-                </div>
-                <div
-                  id="password"
-                  className="flex flex-col gap-[4px] max-sm:text-[#ffffff] max-sm:text-base"
-                >
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={handleChange}
-                    id="password"
-                    placeholder="Password"
-                    className="border-b-[2px] rounded-[4px] p-1 pl-2 focus:border-b-[2px] active:border-[#4F7942] focus:outline-none outline-none w-[250px] text-[10px] border-[#4F7942] max-sm:text-sm max-sm:text-[#000000]"
-                  ></input>
-                </div>
-                <p className="mx-auto mt-3  ">
-                  <Link
-                    to="/forget-password"
-                    className="text-[#4F7942] opacity-70 text-md hover:text-[#355E3B] hover:cursor-pointer max-sm:text-[#ffffff]"
-                  >
-                    Forget Password?
-                  </Link>
-                </p>
-              </form>
+                />
+              </div>
+            </div> */}
+
+            <div className="my-5">
+              <ReusableInput
+                label="Email"
+                type="text"
+                placeholder="Enter your email address"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                icon={MdOutlineMail}
+              />
             </div>
 
-            <div className="flex flex-col mt-3 mx-auto max-sm:mt-2">
-              <button
-                className="text-[#ffffff] hover:text-[#355E3B] max-sm:hover:text-[#ffffff] hover:bg-transparent hover:border-[1px] hover:border-[#355E3B] bg-[#355E3B] text-center px-[55px] py-[7px] max-sm:py-[10px] text-[14px] max-sm:text-[17px] rounded-full mx-auto"
-                type="button"
-                onClick={handleSubmit}
-              >
-                {isLoading ? (
-                  <div className="flex items-center">
-                    <div className="w-4 h-4 border-t-2 border-[#4f7942] border-solid rounded-full animate-spin" />
-                  </div>
-                ) : (
-                  <>Login </>
-                )}
-              </button>
-              <p className="mx-auto mt-1 text-sm max-sm:text-[#ffffff]">
-                don't have an account?
-                <span className="text-[#4F7942] opacity-70 text-sm">
-                  <Link
-                    to="/signup"
-                    className="hover:text-[#355E3B] text-xl max-sm:text-[#ffffff] hover:cursor-pointer"
-                  >
-                    &nbsp;Sign Up
-                  </Link>
-                </span>
-              </p>
+            <div className="my-5">
+              <ReusableInput
+                label="Password"
+                type="password"
+                placeholder="Enter your Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                icon={CiLock}
+              />
             </div>
-          </div>
+
+            {/* <p className="mx-auto mt-3  "> */}
+            <Link
+              to="/forget-password"
+              className="text-[12px] flex justify-end cursor-pointer"
+            >
+              Forget Password?
+            </Link>
+            {/* </p> */}
+
+            <button
+              className="text-[#ffffff] hover:text-[#355E3B] mt-10  hover:bg-transparent hover:border-[1px] hover:border-[#355E3B] bg-[#009B4D] text-center px-[55px] py-[7px]  text-[14px] rounded-[10px]  w-full"
+              type="button"
+              onClick={handleSubmit}
+            >
+              {isLoading ? (
+                <div className="flex justify-center items-center">
+                  <div className="w-4 h-4 border-t-2 border-[#4f7942] border-solid rounded-full animate-spin" />
+                </div>
+              ) : (
+                <>Login </>
+              )}
+            </button>
+          </form>
         </div>
       </div>
-
-      <ModalContainer
-        close={toggleSuccess}
-        show={showSuccess}
-        width={"max-w-xs"}
-      >
-        <div>
-          <p className="text-xl text-center">Success!</p>
-          <p className="text-xl text-center">{data?.message} to your mail</p>
-        </div>
-      </ModalContainer>
     </div>
   );
 };
 
 export default Login;
+
+const ReusableInput = ({
+  label,
+  type,
+  placeholder,
+  name,
+  value,
+  onChange,
+  icon: Icon,
+}) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  return (
+    <div className="mb-3">
+      <label htmlFor="" className="block">
+        {label}
+      </label>
+      <div className="flex items-center gap-4 border-b-[1px] border-[#99999999]">
+        {Icon && <Icon />}
+        <input
+          className="outline-none w-full"
+          type={isPasswordVisible ? "text" : type}
+          placeholder={placeholder}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+        {type === "password" && (
+          <div onClick={togglePasswordVisibility} className="cursor-pointer">
+            {isPasswordVisible ? "👁️" : "🙈"}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
