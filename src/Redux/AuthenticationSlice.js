c6bc440acfe3c21e800899d007cb3f74f11dd3f5
adewobiadetayo1@gmail.com <adewobiadetayo1@gmail.com>
2023-12-19 08:21:13 +0100

    date format and grammar fix

--- src/Redux/AuthenticationSlice.js
+++ src/Redux/AuthenticationSlice.js
@@ -77,16 +77,16 @@ const Login_fun_Service = async (data) => {
 
   try {
     const response = await axios.post(Base_URL, data);
-    toast.success(`${response.data?.message}`, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
+    // toast.success(`${response.data?.message}`, {
+    //   position: "top-right",
+    //   autoClose: 5000,
+    //   hideProgressBar: false,
+    //   closeOnClick: true,
+    //   pauseOnHover: true,
+    //   draggable: true,
+    //   progress: undefined,
+    //   theme: "light",
+    // });
     return response.data;
     // Process the response data here
   } catch (error) {
