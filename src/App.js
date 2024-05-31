0fcc4e6c7811598064384f66afcd077f79be171e
ndukwesamuel <ndukwesamuel23@yahoo.com>
2024-05-31 12:33:42 +0100

    dsdsd

--- src/App.js
+++ src/App.js
@@ -1,75 +1,58 @@
-import './App.css';
-import 'react-toastify/dist/ReactToastify.css';
-
-import React from 'react';
-
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-
-import AdminRoute from './Page/Admin/AdminRoute';
-import FacilitatorRoute from './Page/FacilitatorFolder/FacilitatorRoute';
-import OnboardingRoute from './Page/Onboarding/OnboardingRoute';
-import PageRoutes from './Page/PageRoutes';
-import {
-  AdminPrivateRoute,
-  UserPrivateRoute,
-} from './Page/privateroute/Private';
-import Dashboard from './Page/UserDashboard/Dashboard';
-
-// import './main.scss';
+import { useRef, useEffect } from "react";
+import "./App.css";
+import "@smile_identity/smart-camera-web";
+import axios from "axios"; // Import Axios
 
 function App() {
+  const webRef = useRef(null);
+
+  const handle_post = async (data) => {
+    let token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjIxYzk1YzkxZDY4ZmNkOWY0ZTIyZTkiLCJpYXQiOjE3MTY3MzEyOTIsImV4cCI6MTcxNzU5NTI5Mn0.jO48--yX82RYxOctMJE98__TFHSCGGpEhGDWZphO4iU";
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios
+      .post("http://localhost:8000/api/kyc", data, config) // Adjust the URL as per your requirement
+      .then((response) => {
+        console.log("Post request successful:", response);
+      })
+      .catch((error) => {
+        console.error("Error making POST request:", error);
+      });
+  };
+
+  useEffect(() => {
+    const imagesComputedHandler = (e) => {
+      // console.log(e.detail);
+      handle_post(e.detail);
+    };
+    const webComponent = webRef.current;
+
+    if (webComponent) {
+      webComponent.addEventListener("imagesComputed", imagesComputedHandler);
+    }
+
+    return () => {
+      if (webComponent) {
+        webComponent.removeEventListener(
+          "imagesComputed",
+          imagesComputedHandler
+        );
+      }
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <ToastContainer />
-      <Routes>
-        <Route path="/*" exact element={<PageRoutes />} />
-
-        <Route
-          path="/onboarding/*"
-          exact
-          element={
-            <UserPrivateRoute>
-              <OnboardingRoute />
-            </UserPrivateRoute>
-          }
-        />
-
-        <Route
-          path="/facilitator/*"
-          exact
-          element={
-            <UserPrivateRoute>
-              <FacilitatorRoute />
-            </UserPrivateRoute>
-          }
-        />
-
-        <Route
-          path="/dashboard/*"
-          exact
-          element={
-            <UserPrivateRoute>
-              <Dashboard />
-            </UserPrivateRoute>
-          }
-        />
-
-        <Route
-          path="/admin/*"
-          exact
-          element={
-            <AdminPrivateRoute>
-              <AdminRoute />
-            </AdminPrivateRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <smart-camera-web ref={webRef}></smart-camera-web>
+    </>
   );
 }
 
