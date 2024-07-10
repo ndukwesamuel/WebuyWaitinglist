05a02bab2ced55381dc8e025197e518e10862027
ndukwesamuel <ndukwesamuel23@yahoo.com>
2024-07-10 10:51:12 +0100

    fix all issues

--- src/Redux/AdminRecipteSLice.js
+++ src/Redux/AdminRecipteSLice.js
@@ -1,189 +1,3 @@
-// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import axios from "axios";
-// import { toast } from "react-toastify";
-// import { ErrorFunc } from "../utilities/ApiErrorFun";
-
-// let main_url = process.env.REACT_APP_Url;
-// const initialState = {
-//   Admin_get_all_recipte: null,
-//   Admin_get_all_recipte_isError: false,
-//   Admin_get_all_recipte_isSuccess: false,
-//   Admin_get_all_recipte_isLoading: false,
-//   Admin_get_all_recipte_message: null,
-
-//   Admin_get_single_recipte: null,
-//   Admin_get_single_recipte_isError: false,
-//   Admin_get_single_recipte_isSuccess: false,
-//   Admin_get_single_recipte_isLoading: false,
-//   Admin_get_single_recipte_message: null,
-// };
-
-// const Admin_get_all_recipte_fun_Service = async (token) => {
-//   let Base_URL = main_url + "wallet/receipt";
-
-//   try {
-//     const response = await axios.get(Base_URL, {
-//       headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-
-//     return response.data;
-//     // Process the response data here
-//   } catch (error) {
-//     toast.error(`${error}`, {
-//       position: "top-right",
-//       autoClose: 5000,
-//       hideProgressBar: false,
-//       closeOnClick: true,
-//       pauseOnHover: true,
-//       draggable: true,
-//       progress: undefined,
-//       theme: "light",
-//       className: "Forbidden403",
-//     });
-//     // Handle the error here
-
-//     ErrorFunc(error);
-//     throw error;
-//   }
-// };
-
-// export const Admin_get_all_recipte_fun_ = createAsyncThunk(
-//   "AdminRecipteSLice/Admin_get_all_recipte_fun_",
-//   async (_, thunkAPI) => {
-//     try {
-//       let token = thunkAPI.getState().reducer.AuthenticationSlice.data.data?.token;
-
-//       return await Admin_get_all_recipte_fun_Service(token);
-//     } catch (error) {
-//       const message =
-//         (error.response &&
-//           error.response.data &&
-//           error.response.data.message) ||
-//         error.message ||
-//         error.toString();
-//       return thunkAPI.rejectWithValue(message);
-//     }
-//   }
-// );
-
-// const Admin_get_single_recipte_fun_Service = async (id, token) => {
-//   let Base_URL = main_url + `wallet/receipt/${id}`;
-
-//   try {
-//     const response = await axios.get(Base_URL, {
-//       headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-
-//     console.log({
-//       qqwww: response.data,
-//     });
-
-//     // return response.data;
-//     // Process the response data here
-//   } catch (error) {
-//     toast.error(`${error}`, {
-//       position: "top-right",
-//       autoClose: 5000,
-//       hideProgressBar: false,
-//       closeOnClick: true,
-//       pauseOnHover: true,
-//       draggable: true,
-//       progress: undefined,
-//       theme: "light",
-//       className: "Forbidden403",
-//     });
-//     // Handle the error here
-
-//     ErrorFunc(error);
-//     throw error;
-//   }
-// };
-
-// export const Admin_get_single_recipte_fun = createAsyncThunk(
-//   "AdminRecipteSLice/Admin_get_single_recipte_fun",
-//   async (id, thunkAPI) => {
-//     try {
-//       let token = thunkAPI.getState().reducer.AuthenticationSlice.data.data?.token;
-
-//       return await Admin_get_single_recipte_fun_Service(id, token);
-//     } catch (error) {
-//       const message =
-//         (error.response &&
-//           error.response.data &&
-//           error.response.data.message) ||
-//         error.message ||
-//         error.toString();
-//       return thunkAPI.rejectWithValue(message);
-//     }
-//   }
-// );
-
-// export const AdminRecipteSLice = createSlice({
-//   name: "AdminRecipteSLice",
-//   initialState,
-//   reducers: {},
-
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(Admin_get_all_recipte_fun_.pending, (state) => {
-//         state.Admin_get_all_recipte_isLoading = true;
-//       })
-//       .addCase(Admin_get_all_recipte_fun_.fulfilled, (state, action) => {
-//         state.Admin_get_all_recipte_isLoading = false;
-//         state.Admin_get_all_recipte_isSuccess = true;
-//         state.Admin_get_all_recipte = action.payload;
-//       })
-//       .addCase(Admin_get_all_recipte_fun_.rejected, (state, action) => {
-//         state.Admin_get_all_recipte_isLoading = false;
-//         state.Admin_get_all_recipte_isError = true;
-//         state.Admin_get_all_recipte_message = action.payload;
-//         toast.error(`${state.Admin_get_all_recipte_message}`, {
-//           position: "top-right",
-//           autoClose: 5000,
-//           hideProgressBar: false,
-//           closeOnClick: true,
-//           pauseOnHover: true,
-//           draggable: true,
-//           progress: undefined,
-//           theme: "light",
-//         });
-//       })
-
-//       .addCase(Admin_get_single_recipte_fun.pending, (state) => {
-//         state.Admin_get_single_recipte_isLoading = true;
-//       })
-//       .addCase(Admin_get_single_recipte_fun.fulfilled, (state, action) => {
-//         state.Admin_get_single_recipte_isLoading = false;
-//         state.Admin_get_single_recipte_isSuccess = true;
-//         state.Admin_get_single_recipte = action.payload;
-//       })
-//       .addCase(Admin_get_single_recipte_fun.rejected, (state, action) => {
-//         state.Admin_get_single_recipte_isLoading = false;
-//         state.Admin_get_single_recipte_isError = true;
-//         state.Admin_get_single_recipte_message = action.payload;
-//         toast.error(`${state.Admin_get_single_recipte_message}`, {
-//           position: "top-right",
-//           autoClose: 5000,
-//           hideProgressBar: false,
-//           closeOnClick: true,
-//           pauseOnHover: true,
-//           draggable: true,
-//   }),
-// });
-
-// // Action creators are generated for each case reducer function
-// export const {} = AdminRecipteSLice.actions;
-
-// export default AdminRecipteSLice.reducer;
-
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -201,6 +15,12 @@ const initialState = {
   Admin_update_recipte_isSuccess: false,
   Admin_update_recipte_isLoading: false,
   Admin_update_recipte_message: null,
+
+  Admin_single_recipte: null,
+  Admin_single_recipte_isError: false,
+  Admin_single_recipte_isSuccess: false,
+  Admin_single_recipte_isLoading: false,
+  Admin_single_recipte_message: null,
 };
 
 const Admin_get_all_recipte_fun_Service = async (token) => {
@@ -284,6 +104,9 @@ const Admin_get_single_recipte_fun_Service = async (id, token) => {
     return response.data;
     // Process the response data here
   } catch (error) {
+    console.log({
+      error: error,
+    });
     // Handle the error here
     ErrorFunc(error);
     throw error;
@@ -299,6 +122,9 @@ export const Admin_get_single_recipte_fun = createAsyncThunk(
 
       return await Admin_get_single_recipte_fun_Service(id, token);
     } catch (error) {
+      console.log({
+        error: error?.response,
+      });
       const message =
         (error.response &&
           error.response.data &&
@@ -355,6 +181,29 @@ export const AdminRecipteSLice = createSlice({
         state.Admin_update_recipte_isError = true;
         state.Admin_update_recipte_message = action.payload;
         toast.error(`${state.Admin_update_recipte_message}`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
+      .addCase(Admin_get_single_recipte_fun.pending, (state) => {
+        state.Admin_single_recipte_isLoading = true;
+      })
+      .addCase(Admin_get_single_recipte_fun.fulfilled, (state, action) => {
+        state.Admin_single_recipte_isLoading = false;
+        state.Admin_single_recipte_isSuccess = true;
+        state.Admin_single_recipte = action.payload;
+      })
+      .addCase(Admin_get_single_recipte_fun.rejected, (state, action) => {
+        state.Admin_single_recipte_isLoading = false;
+        state.Admin_single_recipte_isError = true;
+        state.Admin_single_recipte_message = action.payload;
+        toast.error(`${state.Admin_single_recipte_message}`, {
           position: "top-right",
           autoClose: 5000,
           hideProgressBar: false,
